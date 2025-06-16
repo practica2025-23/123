a3bf005df53a66c4c0f4bd1d5dc44931626b8db6
practica2025-23 <airomagister322@gmail.com>
2025-06-16 22:12:39 +0300

    Add files via upload

---
+++ LB3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System;
+
+namespace LB3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var enemy = new Enemy(50);
+            var armoredEnemy = new ArmoredEnemy(50, 10);
+            var wall = new BreakableWall(30);
+
+            var bullet = new Bullet(20);
+            var rocket = new Rocket(40);
+
+            Console.WriteLine("Firing Bullet at Enemy...");
+            bullet.HitTarget(enemy);
+
+            Console.WriteLine("\nFiring Bullet at Armored Enemy...");
+            bullet.HitTarget(armoredEnemy);
+
+            Console.WriteLine("\nFiring Bullet at Wall...");
+            bullet.HitTarget(wall);
+
+            Console.WriteLine("\nFiring Rocket at Enemy...");
+            rocket.HitTarget(enemy);
+
+            Console.WriteLine("\nFiring Rocket at Armored Enemy...");
+            rocket.HitTarget(armoredEnemy);
+
+            Console.WriteLine("\nFiring Rocket at Wall...");
+            rocket.HitTarget(wall);
+        }
+    }
+}
